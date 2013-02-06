e64e477c59651d9f61a7af4d15ad40825a393407
Arthur Gautier <superbaloo@superbaloo.net>
2013-02-06 14:41:53 +0100

    Adds demo leak

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -3,10 +3,60 @@ package controllers
 import play.api._
 import play.api.mvc._
 
+import play.api.libs.iteratee._
+import play.api.libs.concurrent._
+import play.api.libs.EventSource
+
+import scala.concurrent.Future
+import scala.concurrent.duration._
+
+import scala.concurrent.ExecutionContext.Implicits.global
+
+
 object Application extends Controller {
-  
+
   def index = Action {
-    Ok(views.html.index("Your new application is ready."))
+    Ok(views.html.index())
   }
-  
+
+  def streamDummy = Action {
+    val futureEmptyString = Promise.timeout[Option[String]](Some(""), Duration(10,"minutes"))
+
+    val dummyEnumerator = Enumerator.fromCallback1[String](
+      x => futureEmptyString, 
+      () => Logger("application").info("Socket closed"), 
+      (_,_) => Logger("application").info("Socket error"))
+
+    Logger("application").info("Dummy opened")
+    Ok.stream(dummyEnumerator &> EventSource()).withHeaders(CONTENT_TYPE -> "text/event-stream")
+  }
+
+
+  def stream = Action {
+    /**
+     * Generates Input.Empty every 10 second in iteratee flow
+     */
+    def interleaver[E] = Enumerator.checkContinue0{
+      val generator = () => Promise.timeout(Input.Empty, Duration(1, "seconds"))
+      new Enumerator.TreatCont0[E] {
+        def apply[A](loop: Iteratee[E, A] => Future[Iteratee[E,A]], k: Input[E] => Iteratee[E,A]) = generator().flatMap{
+          e => {
+            Logger("application").info("loop")
+            loop(k(e))
+          }
+        }
+      } 
+    }
+
+    val futureEmptyString = Promise.timeout[Option[String]](Some(""), Duration(10,"minutes"))
+    val dummyEnumerator = Enumerator.fromCallback1[String](
+      _ => futureEmptyString,
+      () => Logger("application").info("Socket closed"),
+      (_,_) => Logger("application").info("Socket error"))
+
+    val smarterEnumerator = Enumerator.interleave(interleaver, dummyEnumerator)
+
+    Logger("application").info("Smarter opened")
+    Ok.stream(smarterEnumerator &> EventSource()).withHeaders(CONTENT_TYPE -> "text/event-stream")
+  }
 }
